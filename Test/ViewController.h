5a3c475270ba05f3f38c6ea5b71821a8fbf977ce
秋乃雨弓 <AkinoAmaki@akkino-mbp.lan>
2014-11-14 00:13:30 +0900

    Initial Commit

---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by 秋乃雨弓 on 2014/11/14.
+//  Copyright (c) 2014年 秋乃雨弓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by 秋乃雨弓 on 2014/11/14.
+//  Copyright (c) 2014年 秋乃雨弓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
